78731aef3875a27955c42d2b3a586dd5add25305
Duncan Marsh <dwolfm@gmail.com>
2016-02-08 23:28:53 -0800

    starting to pick apart zshrc and all things it sources, to move into dot

---
+++ all/source/resource.sh
@@ -0,0 +1,5 @@
+# aliases for resourcing source files
+alias rrb="source $HOME/.bashrc"
+alias rrz="source $HOME/.zshrc"
+alias rra="source $MESH/alias.sh"
+alias rrt="tmux source-file $HOME/.tmux.conf"
