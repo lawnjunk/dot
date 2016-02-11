a0c9e618610764e6ab51da2368b0ec193318654f
Duncan Marsh <dwolfm@gmail.com>
2016-02-10 16:54:58 -0800

    modified my oh-my-zsh instller to be my own

--- all/init/oh-my-zsh.sh
+++ all/init/oh-my-zsh.sh
@@ -1,3 +1,16 @@
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -n "$ZSH" ]; then
+  ZSH=~/.oh-my-zsh
+fi
 
+if [ -d "$ZSH" ]; then
+  printf "${YELLOW}You already have Oh My Zsh installed.${NORMAL}\n"
+  printf "You'll need to remove $ZSH if you want to re-install.\n"
+  exit
+fi
+
+env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
+  printf "Error: git clone of oh-my-zsh repo failed\n"
+  exit 1
+}
+            }
