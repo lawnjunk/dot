f8697d0fb04fe887b955d03027654a42e8d244fa
Duncan Marsh <dwolfm@gmail.com>
2016-02-10 17:44:19 -0800

    modified oh-my-zsh and vundle init scripts to only run if not allready installed

--- all/init/oh-my-zsh.sh
+++ all/init/oh-my-zsh.sh
@@ -4,17 +4,18 @@ if [ ! -n "$ZSH" ]; then
 fi
 
 if [ -d "$ZSH" ]; then
-  printf "${YELLOW}You already have Oh My Zsh installed.${NORMAL}\n"
-  printf "You'll need to remove $ZSH if you want to re-install.\n"
+  printf "   You already have Oh My Zsh installed.\n"
+  printf "   You'll need to remove $ZSH if you want to re-install.\n"
 fi
 
-env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
-  printf "Error: git clone of oh-my-zsh repo failed\n"
-}
+if [ ! -d "$ZSH" ]; then 
+  env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
+    printf "Error: git clone of oh-my-zsh repo failed\n"
+  }
+fi
 
-env git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting || {
-  printf "Error: git clone zsh-syntax-highlighting failed\n"
-}
-
-
-
+if [ ! -d "$ZSH/custom/plugins/zsh-syntax-highlighting" ]; then 
+  env git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting || {
+    printf "Error: git clone zsh-syntax-highlighting failed\n"
+  }
+fi
--- all/init/vundle.sh
+++ all/init/vundle.sh
@@ -1,4 +1,9 @@
 # install vundle
 mkdir -p ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -c ":PluginInstall" -c ":q" -c ":q"
+
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ];then 
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null || {
+    echo "failed: to clone down Vundle"
+  }
+  vim -c ":PluginInstall" -c ":q" -c ":q"
+fi
