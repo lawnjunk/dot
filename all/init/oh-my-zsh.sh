72b51b0f4b6f8398dcc6e40e4c159d375e703b54
Duncan Marsh <dwolfm@gmail.com>
2016-02-10 16:48:16 -0800

    added oh-my-zsh and vundle installer scrips

---
+++ all/init/oh-my-zsh.sh
@@ -0,0 +1,3 @@
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
---
+++ all/init/vundle.sh
@@ -0,0 +1,4 @@
+# install vundle
+mkdir -p ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c ":PluginInstall" -c ":q" -c ":q"
