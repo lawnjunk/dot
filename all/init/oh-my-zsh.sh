278ae2befd5766851f8876ecbf2c592234402ee6
Duncan Marsh <dwolfm@gmail.com>
2016-02-10 17:00:43 -0800

    stoped zsh installer from exiting

--- all/init/oh-my-zsh.sh
+++ all/init/oh-my-zsh.sh
@@ -6,17 +6,14 @@ fi
 if [ -d "$ZSH" ]; then
   printf "${YELLOW}You already have Oh My Zsh installed.${NORMAL}\n"
   printf "You'll need to remove $ZSH if you want to re-install.\n"
-  exit
 fi
 
 env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
   printf "Error: git clone of oh-my-zsh repo failed\n"
-  exit 1
 }
 
 env git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/ || {
   printf "Error: git clone zsh-syntax-highlighting failed\n"
-  exit 1
 }
 
 
