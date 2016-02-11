a706d52992069c85174675ad28ca1df3a7d7e789
Duncan Marsh <dwolfm@gmail.com>
2016-02-10 17:02:34 -0800

    fixing zsh-highlighting insaller bug

--- all/init/oh-my-zsh.sh
+++ all/init/oh-my-zsh.sh
@@ -12,7 +12,7 @@ env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
   printf "Error: git clone of oh-my-zsh repo failed\n"
 }
 
-env git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/ || {
+env git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting || {
   printf "Error: git clone zsh-syntax-highlighting failed\n"
 }
 
