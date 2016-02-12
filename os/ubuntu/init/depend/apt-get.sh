ba290288b4c241c87c5afee503e20fd4f4760f27
Duncan Marsh <dwolfm@gmail.com>
2016-02-12 04:23:02 +0000

    added build-essentials to apt-get installer in depend

--- os/ubuntu/init/depend/apt-get.sh
+++ os/ubuntu/init/depend/apt-get.sh
@@ -1,2 +1,2 @@
 apt-get -q -y update 
-apt-get -q -y install vim zsh curl tmux cmake git node cowsay tree nmap
+apt-get -q -y install vim zsh curl tmux cmake git node cowsay tree nmap build-essential
