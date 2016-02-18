4671115717da90755217d9d9ae8a13e1ada16961
Duncan Marsh <dwolfm@gmail.com>
2016-02-18 14:39:55 -0800

    added a line to uniq to prevent from duplicate lines

--- all/source/all/path.sh
+++ all/source/all/path.sh
@@ -1,1 +1,2 @@
-export PATH=$PATH:$DOT/bin:$DOT/all/bin:$DOT/os/$DOT_OS/bin
+PATH=$PATH:$DOT/bin:$DOT/all/bin:$DOT/os/$DOT_OS/bin
+export PATH=$(echo $PATH | tr  ':' "\n" |  sort |  uniq  |tr '\n' ':')
