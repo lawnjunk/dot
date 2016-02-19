748449c022926579dd3cf39a34b7e7e1ca617302
Duncan Marsh <dwolfm@gmail.com>
2016-02-18 23:21:07 -0800

    added nvm init in archlinux

---
+++ os/archlinux/init/general/nvm-init.sh
@@ -0,0 +1,11 @@
+# install nvm
+
+if [ ! -d "$HOME/.nvm" ]; then 
+  env git clone https://github.com/creationix/nvm.git ~/.nvm || {
+    echo "Error: git clone nvm fialed"
+  }  
+
+  pushd . 
+  cd ~/.nvm 
+  git checkout `git describe --abrev=0 --tags`
+fi 
