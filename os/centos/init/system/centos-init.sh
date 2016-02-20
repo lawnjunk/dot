f6ebfca02c3d7d522b53baf105b28050e6c429be
Duncan Marsh <dwolfm@gmail.com>
2016-02-19 18:55:35 -0800

    centos-init: yum now uses sudo

--- os/centos/init/system/centos-init.sh
+++ os/centos/init/system/centos-init.sh
@@ -1,3 +1,3 @@
 echo -e "\n\n RUNNING CENTOS INIT\n\n"
-yum -y  groupinstall "Development Tools"
-yum -y install man tmux nmap vim node tree ack dig cmake cowsay git curl zsh python-devel
+sudo yum -y  groupinstall "Development Tools"
+sudo yum -y install man tmux nmap vim node tree ack dig cmake cowsay git curl zsh python-devel
