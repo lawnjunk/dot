64ac42b445e05d7cdc70219d060800d7893b3db2
Duncan Marsh <dwolfm@gmail.com>
2016-03-31 08:27:15 -0700

    so much

--- all/source/bash/vi-mode.sh
+++
@@ -1,9 +0,0 @@
-set -o vi
-bind -m vi-insert "\C-i":shell-forward-word
-bind -m vi-insert "\C-i":shell-forward-word
-bind -m vi-insert "\C-l":clear-screen
-bind -m vi-insert "\C-b":backward-word
-bind -m vi-insert "\C-e":end-of-line
-bind -m vi-insert "\C-a.":beginning-of-line
-bind -m vi-insert "C-w.":backward-kill-word
-
