5cb7dbf5c634c3804472c053a2b920490e7a08bb
Duncan Marsh <dwolfm@gmail.com>
2016-04-17 20:58:31 -0700

    fixed gosl to use V_GO_VERSION

--- all/source/all/shorts-navigation.sh
+++ all/source/all/shorts-navigation.sh
@@ -5,4 +5,4 @@ alias ..='cd ..'
 alias gitz="cd $HOME/gitz"
 alias bldz="cd $HOME/bldz"
 alias dotz="cd $HOME/.dot"
-alias gosl="cd $HOME/gitz/go/src/github.com/slugbyte"
+alias gosl="cd $HOME/gitz/go/$V_GO_VERSION/src/github.com/slugbyte"
