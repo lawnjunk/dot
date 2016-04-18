7ee6f6161dc46b17b1708bb5f1cb212e020f4e91
Duncan Marsh <dwolfm@gmail.com>
2016-04-17 19:09:34 -0700

    ps1 from func backup now in bin

--- all/source/all/color-man.sh
+++ all/source/all/color-man.sh
@@ -1,4 +1,3 @@
-# man in full color
 man() {
   env \
     LESS_TERMCAP_mb=$(printf "\e[1;31m") \
@@ -7,6 +6,6 @@ man() {
     LESS_TERMCAP_se=$(printf "\e[0m") \
     LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
     LESS_TERMCAP_ue=$(printf "\e[0m") \
-    LESS_TERMCAP_us=$(printf "\e[1;32m") \
-      man "$@"
+    less_termcap_us=$(printf "\e[1;32m") \
+    man "$@"
 }
